400a4190c9f24f9fe5a888d6aebc20a74c189b8b
Bhabani Barik <bhabanishankar.barik21@gmail.com>
2024-01-28 22:24:55 +0530

    Update Js

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+// Generate computer's choice
+const genComputerhoice = () => {
+    const options =["rock", "paper", "scissors"];
+    Math.floor(Math.random() * 3)
+    return options[randIdx];
+}
+
+
 // Generate User's choice 
 const playGame = (userChoice) => {
     console.log("User Choice = ", userChoice);
