26e0eaa73c33c3c3d333d3554fab1513b1a31213
Bhabani Barik <bhabanishankar.barik21@gmail.com>
2024-02-05 21:41:17 +0530

    updated  javascript file

--- app.js
+++ app.js
@@ -5,23 +5,33 @@ const choices = document.querySelectorAll(".choice");
 
 // Generate computer's choice
 const genComputerChoice = () => {
-    const options =["rock", "paper", "scissors"];
-    const randIdx = Math.floor(Math.random() * 3)
-    return options[randIdx];
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
 };
 
-
-// Generate User's choice 
-const playGame = (userChoice) => {
-    console.log("User Choice = ", userChoice);
-    //Generate computer choice 
-    const compChoice = genComputerChoice();
-    console.log("Computer Choice = ", compChoice);
+const drawGame = () => {
+  console.log("Game was draw.");
 }
 
+// Generate User's choice
+const playGame = (userChoice) => {
+  console.log("User Choice = ", userChoice);
+  //Generate computer choice
+  const compChoice = genComputerChoice();
+  console.log("Computer Choice = ", compChoice);
+
+  
+  if (userChoice === compChoice) {
+    //Draw Game
+    drawGame();
+
+  }
+};
+
 choices.forEach((choice) => {
-    choice.addEventListener("click", () => {
-        const userChoice = choice.getAttribute("id");
-        playGame(userChoice);
-    });
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
 });
